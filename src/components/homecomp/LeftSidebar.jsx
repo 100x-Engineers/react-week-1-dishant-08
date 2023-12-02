3fb0c219b73a9b8cf37861783b4128abd7ae44d7
Dishant Sahu <dishant.sahu07@gmail.com>
2023-12-02 23:55:58 +0530

    fixed model

--- src/components/homecomp/LeftSidebar.jsx
+++ src/components/homecomp/LeftSidebar.jsx
@@ -1,4 +1,5 @@
-import Logo from "../../assets/100x-Logo.svg";
+// Import statements
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import Logo100 from "../../assets/copy-link-100.svg";
 import HomeInactive from "../../assets/state-not-selectedhome-icon.svg";
@@ -10,7 +11,7 @@ import Button from "../button";
 import userAvatar from "../../assets/user-avatar.png";
 import ThreeDot from "../../assets/dark-theme-ellipses-group.svg";
 import { createPortal } from "react-dom";
-import TweetModal from "../modal/tweetmodal.jsx";
+import TweetModal from "../modal/tweetmodal"; // Removed .jsx extension
 import { useState, useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 
@@ -19,16 +20,19 @@ function DesktopHome({ page }) {
     <>
       <div>
         <Link to={"/home"} className="flex py-3 px-5 gap-5 ">
-          <img src={page === "home" ? HomeActive : HomeInactive} />
-          <p className=" text-neutral-50 font-Inter text-[1.1875rem] font-medium  ">
+          <img src={page === "home" ? HomeActive : HomeInactive} alt="Home" />
+          <p className="text-neutral-50 font-Inter text-[1.1875rem] font-medium">
             Home
           </p>
         </Link>
       </div>
       <div>
         <Link to={"/user"} className="flex py-3 px-5 gap-5 ">
-          <img src={page === "user" ? IconActive : IconInactive} />
-          <p className=" text-neutral-50 font-Inter text-[1.1875rem] font-medium  ">
+          <img
+            src={page === "user" ? IconActive : IconInactive}
+            alt="Profile"
+          />
+          <p className="text-neutral-50 font-Inter text-[1.1875rem] font-medium">
             Profile
           </p>
         </Link>
@@ -38,20 +42,20 @@ function DesktopHome({ page }) {
 }
 
 export default function LeftSidebar() {
-  // const [showTweetModal, SetShowTweetModal] = useState(false);
   const { showTweetModal, SetShowTweetModal } = useContext(AuthContext);
+
   return (
-    <div className="flex p-5 flex-col h-screen justify-between  border-r border-r-neutral-700 ">
+    <div className="flex p-5 flex-col h-screen justify-between border-r border-r-neutral-700">
       <div className="flex flex-col gap-2">
-        <div className=" py-3 px-5 flex justify-start gap-2.5 self-stretch   ">
-          <div className="flex justify-center items-end gap-0.5 ">
-            <img src={Logo100} alt="100" />
-            <img src={Logox} alt="x" />
+        <div className="py-3 px-5 flex justify-start gap-2.5 self-stretch">
+          <div className="flex justify-center items-end gap-0.5">
+            <img src={Logo100} alt="Logo 100" />
+            <img src={Logox} alt="X" />
           </div>
         </div>
         <DesktopHome page="home" />
-        <div className="p-2.5 ">
-          <div className="py-tx ">
+        <div className="p-2.5">
+          <div className="py-tx">
             <Button
               variant="solidBlue"
               type="medium"
@@ -67,18 +71,18 @@ export default function LeftSidebar() {
       <footer>
         <div className="flex justify-between items-center self-stretch">
           <div className="flex items-start gap-3">
-            <img src={userAvatar} alt="userAvatar" />
+            <img src={userAvatar} alt="User Avatar" />
             <div className="flex flex-col items-start">
-              <p className="text-neutral-50 font-Inter text-fx font-bold ">
+              <p className="text-neutral-50 font-Inter text-fx font-bold">
                 aarushe_reddy
               </p>
-              <p className="text-neutral-500 font-Inter text-fx  ">
+              <p className="text-neutral-500 font-Inter text-fx">
                 @aarushe_reddy
               </p>
             </div>
           </div>
-          <div className=" flex justify-center items-center w-[2.0625rem] h-[2.0625rem] px-[0.49413rem] ">
-            <img src={ThreeDot} alt="ThreeDot" />
+          <div className="flex justify-center items-center w-[2.0625rem] h-[2.0625rem] px-[0.49413rem]">
+            <img src={ThreeDot} alt="Three Dot" />
           </div>
         </div>
       </footer>
