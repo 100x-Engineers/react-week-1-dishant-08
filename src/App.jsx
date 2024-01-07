5983e25b61b3f060ecd4c9bb69f4ce37b7c2ca5b
Dishant Sahu <dishant.sahu07@gmail.com>
2024-01-07 16:08:19 +0530

    vercel speed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import Home from "./pages/home/index.jsx";
 import Step1 from "./pages/Login/step1.jsx";
 import Step2 from "./pages/Login/step2.jsx";
@@ -76,7 +76,6 @@ function App() {
       <AuthProvider>
         <RouterProvider router={router} />
       </AuthProvider>
-      <SpeedInsights />
     </>
   );
 }
